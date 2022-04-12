32a0f1c66c46d30259c764a86af8aab15657c096
klkl <kuanglong0312@sina.com>
2022-04-12 15:06:46 +0800

    fix: 精简模型导入方式, 设置背景

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <div id="webgl"></div>
+  <div class="main">
+    <div id="webgl"></div>
+    <button class="bad-btn" @click="badTheBridge">登机桥损坏</button>
+  </div>
 </template>
 <script lang="ts">
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { CSS2DObject, CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer'
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import * as THREE from 'three'
 import { onMounted, Ref } from 'vue'
+
+interface Position {
+  x: number
+  y: number
+  z: number
+}
+
+interface TextConfig {
+  value: string
+  fontColor: string
+  bgColor: string
+}
 
 export default {
   setup() {
     let scene: THREE.Scene
     let camera: THREE.Camera
     let renderer: THREE.WebGLRenderer
+    let labelRenderer: CSS2DRenderer
     let orbitControls: OrbitControls
-
     // 创建场景
     const addScene = () => {
       scene = new THREE.Scene()
-      const axes = new THREE.AxesHelper(120);
-      scene.add(axes);
+      const axes = new THREE.AxesHelper(120)
+      scene.add(axes)
 
-      const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-      const material = new THREE.MeshNormalMaterial();
+      const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 )
+      const material = new THREE.MeshNormalMaterial()
 
-      const mesh = new THREE.Mesh( geometry, material );
+      const mesh = new THREE.Mesh( geometry, material )
       scene.add( mesh )
     }
 
@@ -40,15 +56,26 @@ export default {
     const addRenderer = () => {
       const canvas = document.querySelector('#webgl')
       renderer = new THREE.WebGLRenderer({
-        antialias: true
+        antialias: true,
+        alpha: true
       })
-      renderer.setClearColor(0xffffff)
-      renderer.setPixelRatio(window.devicePixelRatio);
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      renderer.shadowMap.enabled  = true; //激活阴影
+      renderer.setClearAlpha(0.2)
+      renderer.setPixelRatio(window.devicePixelRatio)
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      renderer.shadowMap.enabled  = true //激活阴影
       renderer.render(scene, camera)
       canvas?.appendChild(renderer.domElement)
     }
+
+    // 2D渲染器构建
+		const add2DRenderer = () => {
+			labelRenderer = new CSS2DRenderer()
+			labelRenderer.setSize(window.innerWidth, window.innerHeight)
+			labelRenderer.domElement.style.position = 'absolute'
+			labelRenderer.domElement.style.top = '0'
+			labelRenderer.domElement.style.pointerEvents = 'none'
+			document.querySelector('#webgl')?.appendChild(labelRenderer.domElement)
+		}
 
     // 创建控制器
     const addControl = () => {
@@ -60,7 +87,7 @@ export default {
       orbitControls.enablePan = true
     }
 
-    function animate() {
+    const animate = () => {
 
       requestAnimationFrame( animate );
 
@@ -77,16 +104,17 @@ export default {
       const ambient = new THREE.AmbientLight(0xffffff);
       scene.add(ambient); // 将环境光添加到场景中
 
-      // 户外光照效果
-      const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff);
-      hemiLight.position.set(0, 200, 0);
-      scene.add(hemiLight);
+      // 点光源
+      const pointLight = new THREE.PointLight('#ffffff', 2.4, 0)
+      pointLight.position.set(0, 200, 0)
+      scene.add(pointLight)
 
       // 太阳光
-      const directionalLight = new THREE.DirectionalLight(0xffffff);
-      directionalLight.position.set(0, 100, 0);
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 2.4);
+      directionalLight.position.set(0, 50, 0);
       directionalLight.castShadow = false;
       scene.add(directionalLight);
+
     }
 
     // 加载过程处理
@@ -99,78 +127,203 @@ export default {
     mtlLoader.setPath('/objs/')
 
     // 添加模型
-    const addModel = (fileName: string, instanceName: string, x: number, y: number, z: number, scale: number = 0.01, instance?: THREE.Group | null | undefined) => {
+    const addModel = (fileName: string, instanceName: string, position: Position, scale: number = 0.01) => {
       mtlLoader.load(fileName + '.mtl', (materials) => {
 				materials.preload()
 				var objLoader = new OBJLoader()
 				objLoader.setMaterials(materials)
 				objLoader.setPath('objs/')
 				objLoader.load(fileName + '.obj', (object) => {
-					object.position.x = x
-					object.rotation.y = y
-          object.position.z = z
+					object.position.x = position.x
+					object.position.y = position.y
+          object.position.z = position.z
 					object.scale.set(scale, scale, scale)
 					object.name = instanceName
-          if (instance) {
-            instance = object
-          }
 					scene.add(object)
 				}, onLoadProgess, onLoadError);
 			})
     }
-    let animatePlane: THREE.Group
-    let bridgeHasColor: THREE.Group
-    const initModel = () => {
-      // 地面
-      addModel('ground', 'ground', -50, 0, -15, 0.05)
-      // 最前端广场
-      addModel('frontGround', 'frontGround', -50, 0, -15, 0.05)
-      // 候机1
-      addModel('wait1', 'wait1', -50, 0, -15, 0.05)
-      // 候机2
-      addModel('wait2', 'wait2', -50, 0, -15, 0.05)
-      // 候机3
-      addModel('wait3', 'wait3', -50, 0, -15, 0.05)
-      // 登机桥
-      var bridgeArr = [
-				{x:-50, z:-15, y: 0},
-				{x:-56, z:-15, y: 0},
-				{x:-48, z:-15, y: 0},
-				{x:-52.8, z:-15, y: 0},
-				{x:-70.5, z:-15, y: 0},
-				{x:-65.1, z:-15, y: 0},
-				{x:-80.5, z:-15, y: 0},
-				{x:-82.5, z:-15, y: 0},
-				{x:-85, z:-15, y: 0},
-				{x:-88, z:-15, y: 0},
-				{x:-65.1, z:-1, y: 0}
-			]
-      bridgeArr.forEach((point, i) => {
-        addModel('bridge1', 'bridge' + i + 1, point.x, point.y, point.z, 0.05)
-      })
-      // 登机桥带拐
-      addModel('bridge2', 'bridge2-1', -50, 0, -15, 0.05)
-      // 飞机
-      addModel('fj', 'plane1', 28.5, 119.6, -3, 0.01)
-      addModel('fj', 'plane2', 24.5, 119.6, -3, 0.01)
-      addModel('fj', 'plane3', 17, 119.6, -3, 0.01)
-      addModel('fj', 'plane4', 9, 119.6, -3, 0.01)
-      addModel('fj', 'animatePlane', 35, 4.695, 21.25, 0.02, animatePlane)
+
+    // 添加提示
+    const addTips = (name: string, textConfig: TextConfig, position: Position) => {
+      const tipsGeo = new THREE.PlaneBufferGeometry(3, 1, 1, 1)
+			const tips = new THREE.Mesh(tipsGeo, createFont(textConfig.value, {bgColor: textConfig.bgColor, fontColor: textConfig.fontColor}))
+			tips.position.set(position.x, position.y, position.z)
+      tips.name = name
+			scene.add(tips)
     }
+
+    const initModel = () => {
+      const renderArr = [
+        {type: 'model', fileName: 'ground', name: 'ground', position: {x: -50, y: 0, z: -15}, scale: 0.05},
+        {type: 'model', fileName: 'frontGround', name: 'frontGround', position: {x: -50, y: 0, z: -15}, scale: 0.05},
+        {type: 'model', fileName: 'wait1', name: 'wait1', position: {x: -50, y: 0, z: -15}, scale: 0.05},
+        {type: 'model', fileName: 'wait2', name: 'wait2', position: {x: -50, y: 0, z: -15}, scale: 0.05},
+        {type: 'model', fileName: 'wait3', name: 'wait3', position: {x: -50, y: 0, z: -15}, scale: 0.05},
+        {type: 'model', fileName: 'bridge1', name: 'bridge1', position: {x:-50, y: 0, z:-15}, scale: 0.05},
+        {type: 'model', fileName: 'bridge1', name: 'bridge2', position: {x:-56, y: 0, z:-15}, scale: 0.05},
+        {type: 'model', fileName: 'bridge1', name: 'bridge3', position: {x:-48, y: 0, z:-15}, scale: 0.05},
+        {type: 'model', fileName: 'bridge1', name: 'bridge4', position: {x:-52.8, y: 0, z:-15}, scale: 0.05},
+        {type: 'model', fileName: 'bridge1', name: 'bridge5', position: {x:-70.5, y: 0, z:-15}, scale: 0.05},
+        {type: 'model', fileName: 'bridge1', name: 'bridge6', position: {x:-65.1, y: 0, z:-15}, scale: 0.05},
+        {type: 'model', fileName: 'bridge1', name: 'bridge7', position: {x:-80.5, y: 0, z:-15}, scale: 0.05},
+        {type: 'model', fileName: 'bridge1', name: 'bridge8', position: {x:-82.5, y: 0, z:-15}, scale: 0.05},
+        {type: 'model', fileName: 'bridge1', name: 'bridge9', position: {x:-85, y: 0, z:-15}, scale: 0.05},
+        {type: 'model', fileName: 'bridge1', name: 'bridge10', position: {x:-88, y: 0, z:-15}, scale: 0.05},
+        {type: 'model', fileName: 'bridge2', name: 'bridge2-1', position:{ x: -48, y: 0, z: -15}, scale: 0.05},
+        {type: 'model', fileName: 'fj', name: 'plane1', position: { x: 28.5, y: 119.6, z: -3}, scale: 0.01},
+        {type: 'model', fileName: 'fj', name: 'plane2', position: { x: 24.5, y: 119.6, z: -3}, scale: 0.01},
+        {type: 'model', fileName: 'fj', name: 'plane3', position: { x: 17, y: 119.6, z: -3}, scale: 0.01},
+        {type: 'model', fileName: 'fj', name: 'plane4', position: { x: 9, y:119.6, z:-3}, scale: 0.01},
+        {type: 'model', fileName: 'fj', name: 'animatePlane', position: { x: 35, y: 4.695, z: 21.25}, scale: 0.02},
+        // 飞机故障提示
+        {type: 'tip', name: 'planeErrorTip', textConfig: { value: '正在检修中...', bgColor: '#ff0000', fontColor: '#ffffff'}, position: { x: 28.5, y: 1.5, z: -3}},
+        // 飞机延误提示
+        {type: 'tip', name: 'putoffTip', textConfig: { value: '晚点 00:40', bgColor: '#ff0000', fontColor: '#ffffff'}, position: { x: 9, y: 1.5, z: -3}},
+        // 登机桥故障提示
+        {type: 'tip', name: 'bridgeErrorTip', textConfig: { value: '机械故障', bgColor: '#ff0000', fontColor: '#ffffff'}, position: { x: 13.8, y: 2, z: 4}}
+      ]
+      renderArr.forEach(info => {
+        switch(info.type) {
+          case 'model':
+            addModel(<string>info.fileName, info.name, info.position, info.scale)
+            break
+          case 'tip':
+            addTips(info.name, <TextConfig>info.textConfig, info.position)
+            break
+          default:
+            break
+        }
+      })
+
+      // 动画飞机跟随的牌子
+			var planeAnimateInfo = document.createElement('div')
+			planeAnimateInfo.className = 'the-animate-modal'
+			planeAnimateInfo.innerHTML = '<div>HU 7635</div>' +
+				'<div>Boeing 787</div>'
+			planeAnimateInfo.classList.add('hide')
+			const planeAnimateModal = new CSS2DObject(planeAnimateInfo)
+      planeAnimateModal.name = 'planeAnimateModal'
+			scene.add(planeAnimateModal)
+    }
+
+		/**
+     * 创建文字贴图
+		 * text 文字
+		 * options.fontColor 文字颜色
+		 * options.bgColor 背景颜色
+		 */
+		const createFont = (text: string, options={bgColor: '', fontColor: ''}) => {
+			const canvas = document.createElement('canvas')
+			canvas.width = 512
+			canvas.height = 128
+			const c = canvas.getContext('2d')
+			// 矩形区域填充背景
+      if (c) {
+        c.fillStyle = options.bgColor || '#ff0000'
+        c.fillRect(0, 0, 512, 128)
+        c.beginPath()
+        // 文字
+        c.beginPath()
+        c.translate(256,64)
+        c.fillStyle = options.fontColor || '#ffffff' //文本填充颜色
+        c.font = 'bold 36px 微软雅黑' //字体样式设置
+        c.textBaseline = 'middle' //文本与fillText定义的纵坐标
+        c.textAlign = 'center' //文本居中(以fillText定义的横坐标)
+        c.fillText(text, 0, 0)
+      }
+			const texture = new THREE.CanvasTexture(canvas)
+			const textMaterial = new THREE.MeshPhongMaterial({
+				map: texture, // 设置纹理贴图
+				// side: THREE.DoubleSide,
+				transparent: true,
+				opacity: 0.9
+			})
+			if (textMaterial.map) {
+        textMaterial.map.needsUpdate = true
+      }
+			return textMaterial
+		}
+
+    		// ===================== 动画 ======================
+		const initAnimate = () => {
+			renderer.render(scene, camera)
+			labelRenderer.render(scene, camera)
+      const movePlane = scene.getObjectByName('animatePlane')
+			if (movePlane) {
+				movePlane.position.x -= 0.2
+				if (movePlane.position.x < -10) {
+					movePlane.position.y += 0.02
+				}
+				if (movePlane.position.x < -100) {
+					movePlane.position.x = 35
+					movePlane.position.y = 0
+				}
+        const planeAnimateModal = <CSS2DObject>scene.getObjectByName('planeAnimateModal')
+        if (planeAnimateModal) {
+          planeAnimateModal.position.set(movePlane.position.x, movePlane.position.y + 4.5, movePlane.position.z)
+				  planeAnimateModal.element.classList.remove('hide')
+        }
+			}
+			requestAnimationFrame(initAnimate)
+		}
 
     const init = () => {
       addScene()
       addCamera()
       addRenderer()
+      add2DRenderer()
       addControl()
       addLights()
       initModel()
+      initAnimate()
+    }
+
+    const badTheBridge = () => {
+      const badBridge = scene.getObjectByName('bridge2-1')
+      badBridge?.traverse(child => {
+        if ( child instanceof THREE.Mesh ) {
+          child.material= new THREE.MeshBasicMaterial({color: 0xFF0000, transparent: true, opacity: 0.8})
+        }
+      })
     }
 
     onMounted(() => {
       init()
       animate()
     })
+
+    return {
+      badTheBridge
+    }
   }
 }
 </script>
+
+<style>
+body,
+head {
+  padding: 0;
+  margin: 0;
+}
+.main {
+  background: url(./assets/ball.jpg) center / cover no-repeat;
+}
+.bad-btn {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+}
+.the-modal,
+.the-animate-modal {
+  padding: 5px;
+  line-height: 20px;
+  pointer-events: none;
+  transition: visibility 0.2s cubic-bezier(0.23, 1, 0.32, 1) 0s, left 0.4s cubic-bezier(0.23, 1, 0.32, 1) 0s, top 0.4s cubic-bezier(0.23, 1, 0.32, 1) 0s;
+  background-color: rgba(255, 255, 255, 0.9);
+  box-shadow: rgb(174, 174, 174) 0px 0px 10px;
+  border-radius: 3px;
+  font-size: 12px;
+  color: rgb(87, 87, 87);
+}
+</style>
