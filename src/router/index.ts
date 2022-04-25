315ef57b3be4754731943095a0a30e801dbdb8ea
klkl <kuanglong0312@sina.com>
2022-04-25 11:27:33 +0800

    feat: add ar test

---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import type { RouteRecordRaw } from 'vue-router'
+import type { App } from 'vue'
+
+import { createRouter, createWebHashHistory } from 'vue-router'
+const basicRoutes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Index',
+    component: () => import('/@/views/Airport.vue')
+  },
+  {
+    path: '/ar',
+    name: 'arPage',
+    component: () => import('/@/views/AR.vue')
+  }
+]
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes: basicRoutes,
+  strict: true,
+  scrollBehavior: () => ({ left: 0, top: 0 }),
+})
+
+export function setupRouter(app: App<Element>) {
+  app.use(router)
+}
---
+++ src/views/AR.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="container">
+    <a-scene mindar-image="imageTargetSrc: ./targets.mind;" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+      <a-assets>
+        <!-- <template v-for="v in ObjsData">
+          <a-asset-item
+            :id="v.name + '-obj'"
+            :src="`./objs/${v.fileName}.obj`"
+          ></a-asset-item>
+          <a-asset-item
+            :id="v.name + '-mtl'"
+            :src="`./objs/${v.fileName}.mtl`"
+          ></a-asset-item>
+        </template> -->
+
+        <a-asset-item
+            id="fj-obj"
+            src="./objs/fj.obj"
+          ></a-asset-item>
+          <a-asset-item
+            id="fj-mtl"
+            src="./objs/fj.mtl"
+          ></a-asset-item>
+      </a-assets>
+      <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+      <a-entity mixin="giant" obj-model="obj: #fj-obj; mtl: #fj-mtl" position="0 0 0" scale="0.05 0.05 0.05"></a-entity>
+    </a-scene>
+  </div>
+</template>
+<script lang="ts" setup>
+import 'mind-ar/dist/mindar-image.prod.js'
+import 'aframe'
+import 'mind-ar/dist/mindar-image-aframe.prod.js'
+// import { ObjsData } from '/@/data/objs'
+</script>
